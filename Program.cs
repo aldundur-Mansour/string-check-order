ea94f0ffe923f12eb051ba8d8b4114f4650d4abb
Mansour-Aldundur <mansour.aldundur@tuwaiq.edu.sa>
2021-04-19 03:38:32 +0300

    add: First Commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections ; 
+
+namespace checkOrder
+{
+    class Program
+    {
+        static bool checkOrder(string value) {
+
+            Stack mystack = new Stack() ; 
+            for (int i= 0 ; i < value.Length ; i++)
+
+            {
+
+                if(mystack.Count == 0)
+                {
+                    mystack.Push(value[i]) ; 
+                }
+                else
+                {
+                    if((char) mystack.Peek() != value[i]) 
+                    {
+                        mystack.Push(value[i]) ; 
+                    } 
+                    else
+                    {
+                         mystack.Pop(); 
+                    }  
+
+                }
+                       
+            }
+            return mystack.Count == 0; 
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(checkOrder("123478888798894321"));
+        }
+    }
+}
+
